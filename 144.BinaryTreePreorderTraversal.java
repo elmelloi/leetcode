e1ef77ae197ad80158ddec14a9bbf02d745cc346
zhenfeng.wang <zhenfeng.wang@woqutech.com>
2021-03-20 12:16:15 +0800

    fix: 去除了先序遍历递归解法无用的部分

--- 144.BinaryTreePreorderTraversal.java
+++ 144.BinaryTreePreorderTraversal.java
@@ -16,24 +16,22 @@
 
 // 解法1：递归
 // 时间：0ms，内存：37MB
+
 class Solution {
-    public List<Integer> preOrder(TreeNode root,List<Integer> result) {
-        if(root == null){
-            return new ArrayList<Integer>();
-        }
-        result.add(root.val);
-        List<Integer> left = preOrder(root.left,result);
-        List<Integer> right = preOrder(root.right,result);
-        return result;
+     public void inorder(TreeNode node,List<Integer> res){
+        if(node == null) return ;
+
+        inorder(node.left,res);
+        res.add(node.val);
+        inorder(node.right,res);
     }
 
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        preOrder(root,result);
-        return result;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root,res);
+        return res;
     }
 }
-
 // 解法2：迭代
 
 
