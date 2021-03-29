b7c670f5119ac394b6c9560d579abd9b4f0980ff
zhenfeng.wang <zhenfeng.wang@woqutech.com>
2021-03-29 15:47:20 +0800

    feat:反转链表迭代解法

--- 206.ReverseLinkedList.java
+++ 206.ReverseLinkedList.java
@@ -1,21 +1,39 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
  */
 
 // 解法1：递归解法
 class Solution {
-     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) return head;
-            ListNode last = reverseList(head.next);
-            head.next.next = head;
-            head.next = null;
-            return last;
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+}
+
+// 解法2：迭代解法
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre, current, next;
+        pre = null;
+        current = head;
+        next = head;
+
+        while (current != null) {
+            next = current.next;
+
+            current.next = pre;
+
+            pre = current;
+            current = next;
+        }
+
+        return pre;
     }
 }
