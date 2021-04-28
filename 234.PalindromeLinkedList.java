38d0be9a1081fc7c53de539b30e9426b6ef357aa
zhenfeng.wang <zhenfeng.wang@woqutech.com>
2021-04-28 11:38:26 +0800

    feat:判断回文链表递归解法

--- 234.PalindromeLinkedList.java
+++ 234.PalindromeLinkedList.java
@@ -3,8 +3,24 @@
  * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
  * ListNode next) { this.val = val; this.next = next; } }
  */
-class Solution {
-    public boolean isPalindrome(ListNode head) {
 
+// 递归解法
+// 空间复杂度O(n)
+class Solution {
+    ListNode left;
+
+    public boolean isPalindrome(ListNode head) {
+        left = head;
+        return traverse(head);
+    }
+
+    public boolean traverse(ListNode right) {
+        if (right == null)
+            return true;
+        boolean res = traverse(right.next);
+        // 后序遍历代码
+        res = res && (right.val == left.val);
+        left = left.next;
+        return res;
     }
 }
